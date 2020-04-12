916b3efd3d63f27103deed39813ed14c953110b6
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-04-12 21:48:40 +0700

    Design logo

---
+++ Car_Showroom/Logo.swift
@@ -0,0 +1,28 @@
+//
+//  Logo.swift
+//  Car_Showroom
+//
+//  Created by Le Minh Khoi on 4/12/20.
+//  Copyright © 2020 Le Minh Khoi. All rights reserved.
+//
+
+import SwiftUI
+
+struct Logo: View {
+    var body: some View {
+        Image("mazda")
+            // add the resizable method
+            // in order to resize the image
+            .resizable()
+            .frame(width:250, height: 250)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct Logo_Previews: PreviewProvider {
+    static var previews: some View {
+        Logo()
+    }
+}
